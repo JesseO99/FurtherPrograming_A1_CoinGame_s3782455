f9a7917cbf0068becbc47a65891cd64766ccf522
Jesse Osrecak <jesse.osrecak@gmail.com>
2019-08-15 16:06:57 +1000

    Coin and Coin pair implementation almost complete. Required hashcode methods for both classes.

--- CoinGame/src/model/CoinImpl.java
+++ CoinGame/src/model/CoinImpl.java
@@ -10,30 +10,83 @@ import model.interfaces.Coin;
  * @author Jesse Osrecak
  *
  */
-public class CoinImpl implements Coin {
+public class CoinImpl implements Coin 
+{
+	private CoinFace coinFace = CoinFace.HEADS;
+	private int coinNumber;
+	public CoinImpl(int coinNumber)
+	{
+		this.coinNumber = coinNumber;
+	}
+	@Override
+	public int getNumber() //Returns Coin Number
+	{
+		return coinNumber;
+	}
 
 	@Override
-	public int getNumber() {
+	public CoinFace getFace() //Returns visible coinFace
+	{
 		// TODO Auto-generated method stub
+		return coinFace;
+	}
+
+	@Override
+	public void flip() //Flips CoinFace
+	{
+		switch(coinFace)
+		{
+			case HEADS:
+				coinFace = CoinFace.TAILS;
+				break;
+			case TAILS:
+				coinFace = CoinFace.HEADS;
+				break;
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean equals(Coin coin) //Compares Coins to see if they show the same face and returns true or false.
+	{
+		return coin.getFace() == coinFace;
+	}
+	
+	@Override
+	public boolean equals(Object coin)//Checks to see if object is coin before cheking objects face.
+	{
+		if(coin instanceof Coin)
+		{
+			return equals(coin);
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	@Override 
+	public int hashCode()
+	{
+		//TODO no method implementation
 		return 0;
 	}
-
+	
 	@Override
-	public CoinFace getFace() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void flip() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean equals(Coin coin) {
-		// TODO Auto-generated method stub
-		return false;
+	public String toString()
+	{
+		String cf = "NoFace";
+		switch(coinFace)
+		{
+		case HEADS:
+			cf = "Heads";
+			break;
+		case TAILS:
+			cf = "Tails";
+			break;
+		}
+		return "coin " + coinNumber +":	"+ cf;
 	}
 
 }
--- CoinGame/src/model/CoinPairImpl.java
+++ CoinGame/src/model/CoinPairImpl.java
@@ -10,24 +10,62 @@ import model.interfaces.CoinPair;
  * @author jeje1
  *
  */
-public class CoinPairImpl implements CoinPair {
-
+public class CoinPairImpl implements CoinPair 
+{
+	private Coin coin1,coin2;
+	public CoinPairImpl(Coin coin1, Coin coin2)
+	{
+		this.coin1 = coin1;
+		this.coin2 = coin2;
+	}
 	@Override
-	public Coin getCoin1() {
-		// TODO Auto-generated method stub
-		return null;
+	public Coin getCoin1() //returnsCoin1
+	{
+		return coin1;
 	}
 
 	@Override
-	public Coin getCoin2() {
-		// TODO Auto-generated method stub
-		return null;
+	public Coin getCoin2() //returns coin 1
+	{
+		return coin2;
 	}
 
 	@Override
-	public boolean equals(CoinPair coinPair) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean equals(CoinPair coinPair) //Compares coin faces to see if they match and returns true or false
+	{
+		if((this.coin1.equals((coinPair.getCoin1())) && (this.coin2.equals(coinPair.getCoin2()))))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	@Override
+	public boolean equals(Object coinPair)//Checks to see if object is a coinPair and then checks coin faces.
+	{
+		if(coinPair instanceof CoinPair)
+		{
+			return equals(coinPair);
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	@Override
+	public String toString()
+	{
+		return coin1.toString() + ", " + coin2.toString();
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return 0;//TODO
 	}
 
 }
